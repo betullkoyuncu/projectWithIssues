a6092c83ea36e3822dd0870c061eb800d1541cbe
betullkoyuncu <117205036+betullkoyuncu@users.noreply.github.com>
2023-10-25 15:33:21 +0300

    Create GGG.groovy

---
+++ GGG.groovy
@@ -0,0 +1,17 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        def a = '' + "Hello World"
+        println(a.substring(0));
+
+        def xAdA = 104
+        println xAdA.getClass()
+        def x_A_f_AA = "Guru99"
+        println x_A_f_AA.getClass()
+
+       for (value in []) {
+            println value
+                                    
+        }
+    }
+    
+}
